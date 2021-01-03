ea480dd9e5756ded5f37a1a870f0598a2890a844
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-03 18:02:52 +0000

    add auto-install.cmake file

---
+++ auto-install.cmake
@@ -0,0 +1,13 @@
+# Accepts a single argument, the path to the library in active development.
+# This will configure, build and install the library.
+# Notes:
+# 1. `execute_process` runs at configure time so you must run the configure
+#    step from the application folder to have these operations execute.
+function(install_it)
+    execute_process(
+        COMMAND cmake -S . -B build
+        WORKING_DIRECTORY ${ARGV0})
+    execute_process(
+        COMMAND cmake --build build --target install
+        WORKING_DIRECTORY ${ARGV0})
+endfunction()
